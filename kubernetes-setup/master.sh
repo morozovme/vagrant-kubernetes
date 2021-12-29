be73774d04f0ce7ca484422794598b8eb2bbd506
mikefrostov <m.e.morozov1@gmail.com>
2021-12-29 06:02:05 -0500

    add k8s hosted gitlabci deployment

--- kubernetes-setup/master.sh
+++ kubernetes-setup/master.sh
@@ -350,3 +350,16 @@ sudo kubectl patch storageclass nfs-client -p '{"metadata": {"annotations":{"sto
 #  guestbook frontend service
 # 
 #  kubectl apply -f https://k8s.io/examples/application/guestbook/frontend-service.yaml
+
+
+# install gitlab to kubernetes
+
+sudo helm repo add gitlab https://charts.gitlab.io/
+
+sudo helm install gitlab gitlab/gitlab \
+  --set global.hosts.domain=gitlab.morozovme.com \
+  --set certmanager-issuer.email=m.e.morozov1@gmail.com
+
+sudo kubectl get ingress -lrelease=gitlab
+sudo echo "------passwd for gitlabci:"
+sudo kubectl get secret gitlab-gitlab-initial-root-password -ojsonpath='{.data.password}' | base64 --decode ; echo
