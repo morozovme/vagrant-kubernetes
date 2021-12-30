8574834ad8ad03e50d238d2c3fbdb66c2808bc49
mikefrostov <m.e.morozov1@gmail.com>
2021-12-30 05:55:16 -0500

    comment out gitlabci installation

--- kubernetes-setup/master.sh
+++ kubernetes-setup/master.sh
@@ -354,12 +354,12 @@ sudo kubectl patch storageclass nfs-client -p '{"metadata": {"annotations":{"sto
 
 # install gitlab to kubernetes
 
-sudo helm repo add gitlab https://charts.gitlab.io/
+#sudo helm repo add gitlab https://charts.gitlab.io/
 
-sudo helm install gitlab gitlab/gitlab \
-  --set global.hosts.domain=gitlab.morozovme.com \
-  --set certmanager-issuer.email=m.e.morozov1@gmail.com
+#sudo helm install gitlab gitlab/gitlab \
+#  --set global.hosts.domain=gitlab.morozovme.com \
+#  --set certmanager-issuer.email=m.e.morozov1@gmail.com
 
-sudo kubectl get ingress -lrelease=gitlab
-sudo echo "------passwd for gitlabci:"
-sudo kubectl get secret gitlab-gitlab-initial-root-password -ojsonpath='{.data.password}' | base64 --decode ; echo
+#sudo kubectl get ingress -lrelease=gitlab
+#sudo echo "------passwd for gitlabci:"
+#sudo kubectl get secret gitlab-gitlab-initial-root-password -ojsonpath='{.data.password}' | base64 --decode ; echo
