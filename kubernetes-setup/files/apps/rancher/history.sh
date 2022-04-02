c4eb6ee3d2e39b4f16d2eb99c3997da8a72ab646
morozovme <m.e.morozov1@gmail.com>
2022-04-02 09:35:59 -0400

    fix rancher install script

--- kubernetes-setup/files/apps/rancher/history.sh
+++ kubernetes-setup/files/apps/rancher/history.sh
@@ -1,20 +1,19 @@
-    1  kubectl get nodes
-    2  kubectl get pods --all-namespaces
-    3  helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
-    4  kubectl create namespace cattle-system
-    5  touch cert-manager.sh
-    6  vi cert-manager.sh
-    7  chmod +x cert-manager.sh
-    8  ./cert-manager.sh
-    9  kubectl get pods --namespace cert-manager
-   10  helm install rancher rancher-latest/rancher   --namespace cattle-system   --set hostname=rancher.my.org
-   11  kubectl get pods --all-namespaces
-   12  kubectl get svc
-   13  kubectl get svc --all-namespaces
-   14  kubectl edit svc rancher -n cattle-system
-   15  cat vagrant-kubernetes/kubernetes-setup/files/apps/jenkins/jenkins-service.yaml
-   16  kubectl get svc --all-namespaces
-   17  kubectl edit svc rancher -n cattle-system
-   18  kubectl get svc --all-namespaces
-   19  kubectl get secret --namespace cattle-system bootstrap-secret -o go-template='{{.data.bootstrapPassword|base64decode}}{{"\n"}}'
-   20  history
+#!/bin/bash
+
+set -e
+
+kubectl get nodes
+kubectl get pods --all-namespaces
+helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
+kubectl create namespace cattle-system
+chmod +x cert-manager.sh
+./cert-manager.sh
+kubectl get pods --namespace cert-manager
+helm install rancher rancher-latest/rancher   --namespace cattle-system   --set hostname=rancher.k8s-mater.home
+kubectl get pods --all-namespaces
+kubectl get svc --all-namespaces
+
+# kubectl edit svc rancher -n cattle-system <--- change ClusterIP to LoadBalancer so the metallb gives and IP address
+
+kubectl get secret --namespace cattle-system bootstrap-secret -o go-template='{{.data.bootstrapPassword|base64decode}}{{"\n"}}'
+
