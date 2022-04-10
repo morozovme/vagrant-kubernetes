7ebf846f658d9eaf1eed474f84c3381754eec87d
morozovme <m.e.morozov1@gmail.com>
2022-04-10 06:34:53 -0400

    add jenkins values example

--- kubernetes-setup/files/apps/jenkins/jenkins.sh
+++ kubernetes-setup/files/apps/jenkins/jenkins.sh
@@ -2,9 +2,14 @@
 
 set -e
 
-
+# using manifests
 kubectl create namespace jenkins
 kubectl create -f jenkins.yaml --namespace jenkins
 kubectl create -f jenkins-service.yaml --namespace jenkins
 
-
+# using helm
+#kubectl create namespace jenkins
+#helm repo add jenkins https://charts.jenkins.io
+#helm repo update
+#helm install -f values.yaml jenkins/jenkins --generate-name --namespace jenkins
+#helm show values jenkins/jenkins
