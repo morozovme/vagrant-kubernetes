0546ceb80e90986c64ad02b525327f0141847d61
morozovme <m.e.morozov1@gmail.com>
2022-04-10 19:32:49 +0000

    k8s runner works

--- kubernetes-setup/files/apps/github-actions-runner/install.sh
+++ kubernetes-setup/files/apps/github-actions-runner/install.sh
@@ -9,7 +9,7 @@ set -e
 
 helm repo add actions-runner-controller https://actions-runner-controller.github.io/actions-runner-controller
 helm upgrade --install --namespace actions-runner-system --create-namespace \
-             --wait actions-runner-controller actions-runner-controller/actions-runner-controller
+             --wait actions-runner-controller actions-runner-controller/actions-runner-controller -f values.yaml
 
 
 
