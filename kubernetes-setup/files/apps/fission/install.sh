38750ed867651cef0e8d57f3ac1e3f76031a20c9
morozovme <m.e.morozov1@gmail.com>
2022-04-24 07:22:29 +0000

    add fission example

--- kubernetes-setup/files/apps/fission/install.sh
+++ kubernetes-setup/files/apps/fission/install.sh
@@ -6,3 +6,11 @@ helm repo update
 helm install --version v1.15.1 --namespace $FISSION_NAMESPACE fission \
 	  --set serviceType=NodePort,routerServiceType=NodePort \
 	    fission-charts/fission-all
+
+sleep 20s
+curl -Lo fission https://github.com/fission/fission/releases/download/v1.15.1/fission-v1.15.1-linux-amd64 \
+	    && chmod +x fission && sudo mv fission /usr/local/bin/
+fission env create --name nodejs --image fission/node-env
+curl -LO https://raw.githubusercontent.com/fission/examples/main/nodejs/hello.js
+fission function create --name hello-js --env nodejs --code hello.js
+fission function test --name hello-js
